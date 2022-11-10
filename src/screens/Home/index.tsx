28d975b38809d7b74726276fac21ab275610fe66
ThiagoCosta <thiagohrcosta>
2022-11-10 19:45:25 -0300

    ADding header content

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 import {
  Container,
- Header
+ Header,
+ HeaderContent,
+ TotalCars
 } from './styles';
 
 import Logo from '../../assets/logo.svg';
@@ -17,10 +20,16 @@ export function Home() {
       backgroundColor="transparent"
     />
     <Header>
-      <Logo />
-
-
-    </Header>
+      <HeaderContent>
+        <Logo
+          width={RFValue(108)}
+          height={RFValue(12)}
+        />
+        <TotalCars>
+          Total de 12 carros
+        </TotalCars>
+      </HeaderContent>
+   </Header>
   </Container>
  );
 }
