da12470fbdadf8a296ece2483e1067be3f1b438d
ThiagoCosta <thiagohrcosta>
2022-11-13 11:29:02 -0300

    Adding style to slider

--- App.jsx
+++ App.jsx
@@ -15,6 +15,7 @@ import {
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
+import { CarDetails } from './src/screens/CarDetails';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
