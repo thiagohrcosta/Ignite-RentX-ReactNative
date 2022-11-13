72e40c7d62af9cb756436b8c119c3976c403c6d7
ThiagoCosta <thiagohrcosta>
2022-11-13 11:35:30 -0300

    Adding style to car header

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,9 +3,17 @@ import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
 import {
- Container,
- Header,
- CarImages
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price
 } from './styles';
 
 export function CarDetails() {
@@ -19,6 +27,20 @@ export function CarDetails() {
         imagesUrl={['https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png']}
       />
     </CarImages>
+    <Content>
+      <Details>
+        <Description>
+          <Brand>Lamborghini</Brand>
+          <Name>Huracan</Name>
+        </Description>
+
+        <Rent>
+          <Period>Ao dia</Period>
+          <Price>R$ 580</Price>
+        </Rent>
+      </Details>
+
+    </Content>
  </Container>
  );
 }
