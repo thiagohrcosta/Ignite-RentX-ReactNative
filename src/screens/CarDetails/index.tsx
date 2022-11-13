095cbf486441ca4608e2605108f498bcb8b206ef
ThiagoCosta <thiagohrcosta>
2022-11-13 11:45:56 -0300

    Adding details to car card

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Acessory } from '../../components/Acessory';
+
+import speedSvg from '../../assets/speed.svg';
+import accelerationSvg from '../../assets/acceleration.svg';
+import forceSvg from '../../assets/force.svg';
+import gasolineSvg from '../../assets/gasoline.svg';
+import exchangeSvg from '../../assets/exchange.svg';
+import peopleSvg from '../../assets/people.svg';
+
 
 import {
   Container,
@@ -14,7 +24,8 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Accessories
 } from './styles';
 
 export function CarDetails() {
@@ -40,6 +51,14 @@ export function CarDetails() {
           <Price>R$ 580</Price>
         </Rent>
       </Details>
+      <Accessories>
+        <Acessory name="380Km/h" icon={speedSvg}/>
+        <Acessory name="3.2s" icon={accelerationSvg}/>
+        <Acessory name="800 HP" icon={forceSvg}/>
+        <Acessory name="Gasolina" icon={gasolineSvg}/>
+        <Acessory name="Auto" icon={exchangeSvg}/>
+        <Acessory name="2 pessoas" icon={peopleSvg}/>
+      </Accessories>
       <About>
         Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
         É um belíssimo carro para quem gosta de acelerar.
