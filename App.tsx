469b7ea9b52fe770f77f0626960f9feb4c58e35d
ThiagoCosta <thiagohrcosta>
2022-11-22 19:32:30 -0300

    Creating routes

--- App.tsx
+++ App.tsx
@@ -17,10 +17,8 @@ import {
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
-import { CarDetails } from './src/screens/CarDetails';
-import { Scheduling } from './src/screens/Scheduling';
-import { SchedulingDetails } from './src/screens/SchedulingDetails';
-import { SchedulingComplete } from './src/screens/SchedulingComplete';
+
+import { Routes } from './src/components/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -37,7 +35,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingComplete />
+      <Routes />
     </ThemeProvider>
   );
 }
